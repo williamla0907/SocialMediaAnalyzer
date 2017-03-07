3f20fa67ac92d85bfc398fdcff673ce95eabafa4
William La <williamla0907@gmail.com>
2017-03-06 22:31:16 -0800

    ready

--- SocialMediaAnalyzer/settings.py
+++ SocialMediaAnalyzer/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
+# Extra places for collectstatic to find stati
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
