fb6c9a0c5aee8566c5528e2f784446f48893da15
William La <williamla0907@gmail.com>
2017-03-06 22:26:51 -0800

    heroku ready

--- SocialMediaAnalyzer/settings.py
+++ SocialMediaAnalyzer/settings.py
@@ -16,6 +16,14 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
